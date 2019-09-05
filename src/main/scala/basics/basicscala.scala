2f0636a6ddfe4cb17e6d32d6ea99cff12f1cc857
2016-08-23 <jphilli3@xena07.cs.trinity.edu>
2019-09-05 12:39:25 -0500

    Progress

--- src/main/scala/basics/basicscala.scala
+++ src/main/scala/basics/basicscala.scala
@@ -3,16 +3,56 @@ import swiftvis2.plotting._
 import swiftvis2.plotting.styles.ScatterStyle
 import swiftvis2.plotting.renderer.SwingRenderer
 
-//Cases
-case class countryEdu()
-case class countryGDP()
-case class countryLat()
+//MARK: Cases
+
+/*"Country Name","Country Code","Indicator Name","Indicator Code","1960","1961","1962","1963","1964","1965","1966","1967","1968","1969","1970","1971","1972","1973","1974","1975","1976","1977","1978","1979","1980","1981","1982","19
+83","1984","1985","1986","1987","1988","1989","1990","1991","1992","1993","1994","1995","1996","1997","1998","1999","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015",
+"2016","2017"*/
+case class countryEdu(countryName: String, countryCode: String, indicatorName: String, indicatorCode: String, years: Array[Double])
+
+// country acronym, latitude, longitude, name
+case class countryGDP(countryAcro: String, countryLat: Double, countryLon: Double, countryName: String)
+
+// location_id,location_code,location_name,year,age_group_id,age_group_name,sex_id,sex_name,metric,unit,mean,upper,lower
+case class countryLat(locationId: Int, countryAcro: String, countryName: String, year: String, ageGroupId: Int, ageGroupName: String, sexId: Int, sexName: String, metric: String, unit: String, mean: Double, upper: Double, lower: Double)
 
 
 object Basics {
 
-    def parseLine(line: String): countryEdu = {
-        countryEdu()
+    def parseEdu(line: String): countryEdu = {
+        val (left,right) = line.splitAt(3)
+            countryEdu(
+                left(0).toString, //countryName
+                left(1).toString, //countryCode
+                left(2).toString, //indicatorName
+                left(3).toString, //indicatorCode
+                right.split(",").map(_.toDouble) //years
+            )
+    }
+
+    def parseGDP(line: String): countryGDP = {
+        val p = line.split(",")
+            countryGDP(
+                p(0).toString, //countryAcro
+                p(1).toDouble, //countryLat
+                p(2).toDouble, //countryLon
+                p(3).toString, //countryName
+            )
+    }
+
+    def parseLat(line: String): countryLat = {
+        val p = line.split(",")
+            countryLat(
+                p(0).toInt,
+                p(1).toString,
+                p(2).toString,
+                p(3).toString,
+                p(4).toInt,
+                p(5).toString,
+                p(6).toInt,
+                p(7).toString,
+                p(8).toString,
+            )
     }
 
     def main(args:Array[String]): Unit = {
