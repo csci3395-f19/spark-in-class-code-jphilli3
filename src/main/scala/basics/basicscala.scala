a3457944b40b3e92943b0dac20e7970bbe27ca70
2016-08-23 <jphilli3@atlas04.cs.trinity.edu>
2019-09-05 11:19:07 -0500

    Setup

--- src/main/scala/basics/basicscala.scala
+++ src/main/scala/basics/basicscala.scala
@@ -3,14 +3,26 @@ import swiftvis2.plotting._
 import swiftvis2.plotting.styles.ScatterStyle
 import swiftvis2.plotting.renderer.SwingRenderer
 
+//Cases
+case class countryEdu()
+case class countryGDP()
+case class countryLat()
+
+
 object Basics {
 
-    val countryEdu = scala.io.Source.fromFile("/users/mlewis/workspaceF18/CSCI3395-F18/data/BasicScala/API_NY.GDP.PCAP.KD_DS2_en_csv_v2_10081022.csv")
-    val countryGDP = scala.io.Source.fromFile("/users/mlewis/workspaceF18/CSCI3395-F18/data/BasicScala/countries.tsv")
-    val countryLats = scala.io.Source.fromFile("/users/mlewis/workspaceF18/CSCI3395-F18/data/BasicScala/IHME_GLOBAL_EDUCATIONAL_ATTAINMENT_1970_2015_Y2015M04D27.CSV")
+    def parseLine(line: String): countryEdu = {
+        countryEdu()
+    }
+
+    def main(args:Array[String]): Unit = {
+
+    val sourceEdu = scala.io.Source.fromFile("/users/mlewis/workspaceF18/CSCI3395-F18/data/BasicScala/API_NY.GDP.PCAP.KD_DS2_en_csv_v2_10081022.csv")
+    val sourceGDP = scala.io.Source.fromFile("/users/mlewis/workspaceF18/CSCI3395-F18/data/BasicScala/countries.tsv")
+    val sourceLat = scala.io.Source.fromFile("/users/mlewis/workspaceF18/CSCI3395-F18/data/BasicScala/IHME_GLOBAL_EDUCATIONAL_ATTAINMENT_1970_2015_Y2015M04D27.CSV")
 
     //#1 The Number of value types reported in the education file in the metric column.
     
-
+    }
 
 }
